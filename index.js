1b8124f5db083f3dad6227bb0ea137fc4f219550
Josh Harvey <josh@lamassu.is>
2016-07-31 15:06:07 +0300

    add publishing, authorizing

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 exports.NAME = 'FakeWallet'
 exports.SUPPORTED_MODULES = ['wallet']
 
+const SECONDS = 1000
+const UNSEEN_TIME = 6 * SECONDS
+const PUBLISH_TIME = 12 * SECONDS
+const AUTHORIZE_TIME = 60 * SECONDS
+
+let t0
+
 exports.config = function config () {}
 
 exports.balance = function balance (callback) {
@@ -14,9 +21,16 @@ exports.sendBitcoins = function sendBitcoins (address, satoshis, fee, callback) 
 }
 
 exports.newAddress = function newAddress (info, callback) {
+  t0 = Date.now()
   callback(null, '<Fake address, don\'t send>')
 }
 
 exports.getStatus = function getStatus (toAddress, requested) {
+  const elapsed = Date.now() - t0
+
+  if (elapsed < UNSEEN_TIME) return Promise.resolve({status: 'notSeen'})
+  if (elapsed < PUBLISH_TIME) return Promise.resolve({status: 'published'})
+  if (elapsed < AUTHORIZE_TIME) return Promise.resolve({status: 'authorized'})
+
   return Promise.resolve({status: 'confirmed'})
 }
