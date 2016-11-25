4934eb422cab6672dc68a7ba154653fc9a3850fc
Josh Harvey <josh@lamassu.is>
2016-11-25 18:35:40 +0200

    update for latest lamassu-server

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-'use strict'
-
-exports.NAME = 'FakeWallet'
-exports.SUPPORTED_MODULES = ['wallet']
+const NAME = 'FakeWallet'
+const SUPPORTED_MODULES = ['wallet']
 
 const SECONDS = 1000
 const UNSEEN_TIME = 6 * SECONDS
@@ -10,22 +8,24 @@ const AUTHORIZE_TIME = 60 * SECONDS
 
 let t0
 
-exports.config = function config () {}
+function config () {}
 
-exports.balance = function balance (callback) {
-  callback(null, {BTC: Math.round(1e8 * 10), ETH: Math.round(1e18 * 10)})
+function balance () {
+  return Promise.resolve({BTC: Math.round(1e8 * 10), ETH: Math.round(1e18 * 10)})
 }
 
-exports.sendBitcoins = function sendBitcoins (address, satoshis, fee, cryptoCode, callback) {
-  setTimeout(() => callback(null, '<txHash>'), 2000)
+function sendCoins (address, satoshis, fee, cryptoCode, callback) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve('<txHash>'), 2000)
+  })
 }
 
-exports.newAddress = function newAddress (info, callback) {
+function newAddress (info) {
   t0 = Date.now()
-  callback(null, '<Fake address, don\'t send>')
+  return Promise.resolve('<Fake address, don\'t send>')
 }
 
-exports.getStatus = function getStatus (toAddress, requested) {
+function getStatus (toAddress, requested) {
   const elapsed = Date.now() - t0
 
   if (elapsed < UNSEEN_TIME) return Promise.resolve({status: 'notSeen'})
@@ -33,4 +33,14 @@ exports.getStatus = function getStatus (toAddress, requested) {
   if (elapsed < AUTHORIZE_TIME) return Promise.resolve({status: 'authorized'})
 
   return Promise.resolve({status: 'confirmed'})
+}
+
+module.exports = {
+  NAME,
+  SUPPORTED_MODULES,
+  config,
+  balance,
+  sendCoins,
+  newAddress,
+  getStatus
 }
