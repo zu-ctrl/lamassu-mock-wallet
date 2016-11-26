e9b8197eea7d3eee49751e2f7239b49b3be109b1
Josh Harvey <josh@lamassu.is>
2016-11-27 01:08:41 +0200

    latest l-s

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const NAME = 'FakeWallet'
-const SUPPORTED_MODULES = ['wallet']
 
 const SECONDS = 1000
 const UNSEEN_TIME = 6 * SECONDS
@@ -8,24 +7,22 @@ const AUTHORIZE_TIME = 60 * SECONDS
 
 let t0
 
-function config () {}
-
 function balance () {
   return Promise.resolve({BTC: Math.round(1e8 * 10), ETH: Math.round(1e18 * 10)})
 }
 
-function sendCoins (address, satoshis, fee, cryptoCode, callback) {
+function sendCoins () {
   return new Promise(resolve => {
     setTimeout(() => resolve('<txHash>'), 2000)
   })
 }
 
-function newAddress (info) {
+function newAddress () {
   t0 = Date.now()
   return Promise.resolve('<Fake address, don\'t send>')
 }
 
-function getStatus (toAddress, requested) {
+function getStatus () {
   const elapsed = Date.now() - t0
 
   if (elapsed < UNSEEN_TIME) return Promise.resolve({status: 'notSeen'})
@@ -37,8 +34,6 @@ function getStatus (toAddress, requested) {
 
 module.exports = {
   NAME,
-  SUPPORTED_MODULES,
-  config,
   balance,
   sendCoins,
   newAddress,
