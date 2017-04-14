12f150801c83d1bd275ddfe977d0015472fe7346
Josh Harvey <josh@lamassu.is>
2017-04-14 12:34:36 +0300

    add sweep functionality

--- index.js
+++ index.js
@@ -3,23 +3,40 @@ const BigNumber = require('bignumber.js')
 const NAME = 'FakeWallet'
 
 const SECONDS = 1000
-const UNSEEN_TIME = 6 * SECONDS
-const PUBLISH_TIME = 12 * SECONDS
+const UNSEEN_TIME = 3 * SECONDS
+const PUBLISH_TIME = 9 * SECONDS
 const AUTHORIZE_TIME = 60 * SECONDS
+const BALANCE = 0.01
+const REAL_BALANCE = BALANCE / 2
 
 let t0
 
+function multiplier (cryptoCode) {
+  if (cryptoCode === 'BTC') return new BigNumber(1e8)
+  if (cryptoCode === 'ETH') return new BigNumber(1e18)
+  throw new Error('Unsupported crypto: ' + cryptoCode)
+}
+
 function balance (account, cryptoCode) {
   return Promise.resolve()
-  .then(() => {
-    if (cryptoCode === 'BTC') return new BigNumber(1e8 * 10)
-    if (cryptoCode === 'ETH') return new BigNumber(1e18 * 10)
-    throw new Error('Unsupported crypto: ' + cryptoCode)
-  })
+  .then(() => multiplier(cryptoCode).mul(BALANCE))
 }
 
 function sendCoins (account, toAddress, cryptoAtoms, cryptoCode) {
+  console.log('[%s] DEBUG: Mock wallet about to send %s cryptoAtoms to %s',
+    cryptoCode, cryptoAtoms.toString(), toAddress)
+
   return new Promise(resolve => {
+    const realBalance = multiplier(cryptoCode).mul(REAL_BALANCE)
+    console.log('DEBUG300: %s', realBalance)
+
+    if (cryptoAtoms.gte(realBalance)) {
+      const err = new Error('Insufficient Funds')
+      err.name = 'InsufficientFunds'
+
+      throw err
+    }
+
     setTimeout(() => {
       console.log('[%s] DEBUG: Mock wallet sending %s cryptoAtoms to %s',
         cryptoCode, cryptoAtoms.toString(), toAddress)
@@ -44,10 +61,15 @@ function getStatus (account, toAddress, cryptoAtoms, cryptoCode) {
   return Promise.resolve({status: 'confirmed'})
 }
 
+function sweep () {
+  return Promise.resolve('<txHash>')
+}
+
 module.exports = {
   NAME,
   balance,
   sendCoins,
   newAddress,
-  getStatus
+  getStatus,
+  sweep
 }
